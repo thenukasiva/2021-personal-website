b56475f2820eea1c47af9fde9ceb803281e1f1a8
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2021-09-12 22:51:46 -0400

    update nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -73,7 +73,7 @@ const Nav = (): JSX.Element => {
           gridGap="2rem"
           alignItems="center"
           justifyItems="center"
-          gridTemplateColumns="repeat(5, auto)"
+          gridTemplateColumns="repeat(4, auto)"
           style={{
             borderRadius: '25px',
             background: 'rgba(0, 0, 0, 0.04)',
@@ -96,7 +96,6 @@ const Nav = (): JSX.Element => {
           <NavLink href="/about">about</NavLink>
           <NavLink href="/work">work</NavLink>
           <NavLink href="/projects">projects</NavLink>
-          <NavLink href="/blog">blog</NavLink>
         </Grid>
       </Container>
       <Container alignContent="flex-end" display={['none', 'none', 'flex']}>
