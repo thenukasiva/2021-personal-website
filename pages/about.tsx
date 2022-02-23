47a2fce982e0946654547f9ef6439e15c94160d4
Thenuka Siva <38299804+thenukasiva@users.noreply.github.com>
2022-02-22 19:12:00 -0500

    add new edxperiences

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,43 +1,49 @@
-import React from 'react';
-import { Title, Text, Container, Link, Button } from '@components';
-import Head from 'next/head';
+import React from "react";
+import { Title, Text, Container, Link, Button } from "@components";
+import Head from "next/head";
 
-
-const About = (): JSX.Element =>  (
-    <Container>
-      <Head>
-        <title>about.</title>
-      </Head>
-      <Container alignContent="center" alignItems="center">
-        <Title fontSize={['3rem', '4rem']} as="h2">
-          about me.👩🏾‍💻
-        </Title>
-        <Container maxWidth={['100%', '700px']} marginY="2rem">
-          <Text>
-          I'm a 2A Computer Science student at the <b>University of Waterloo. </b>
-          I am passionate about learning innovative technologies and making projects that have a positive social impact. 
-          </Text>
-          <Text>
-          I just wrapped up my first co-op term at {''} 
-          <a href="https://www.uptake.com">Uptake</a> as a <b>Full-Stack Developer Intern </b>
-          where I worked with <b>SQL</b>, <b>Angular</b> and <b>ASP.NET</b> to help build new features, fix customer reported issues and refactor code.
-          </Text>
-          <Text>
-          Aside from my coding experience, 
-          I have diverse range of leadership and volunteer work 
-          that have helped me grow into the person I am today. 
-          In the past, I was the Co-Founder of my high school's 
-          <b> Amnesty International Club </b>and the Marketing Chair at <b>Sustainable 
-          Youth Ottawa</b> where I led and executed events that directly 
-          gave back to the community.
-          </Text>
-          <Text>When I'm not busy coding or completing schoolwork, I enjoy exploring vegan dishes, learning new instruments, and making spotify playlists!
-          </Text>
-        </Container>
-        <Link href="/work">
-          <Button>My experience</Button>
-        </Link>
+const About = (): JSX.Element => (
+  <Container>
+    <Head>
+      <title>about.</title>
+    </Head>
+    <Container alignContent="center" alignItems="center">
+      <Title fontSize={["3rem", "4rem"]} as="h2">
+        about me.👩🏾‍💻
+      </Title>
+      <Container maxWidth={["100%", "700px"]} marginY="2rem">
+        <Text>
+          I'm a 2B Computer Science student at the{" "}
+          <b>University of Waterloo. </b>I am passionate about learning
+          innovative technologies and making projects that have a positive
+          social impact.
+        </Text>
+        <Text>
+          I'm currently on my second co-op term at {""}
+          <a href="https://www.imaginecommunications.com">
+            Imagine Communications
+          </a>{" "}
+          as a <b>Front-end Developer Intern </b>
+          where I am working with <b>AngularJS</b>, <b>Typescript</b> and{" "}
+          <b>Vue</b> to help build new features to production.
+        </Text>
+        <Text>
+          Aside from my coding experience, I have diverse range of leadership
+          and volunteer work that have helped me grow into the person I am
+          today. In the past, I was the Co-Founder of my high school's
+          <b> Amnesty International Club </b>and the Marketing Chair at{" "}
+          <b>Sustainable Youth Ottawa</b> where I led and executed events that
+          directly gave back to the community.
+        </Text>
+        <Text>
+          When I'm not busy coding or completing schoolwork, I enjoy exploring
+          vegan dishes, learning new instruments, and making spotify playlists!
+        </Text>
       </Container>
-      </Container>
-  );
+      <Link href="/work">
+        <Button>My experience</Button>
+      </Link>
+    </Container>
+  </Container>
+);
 export default About;
--- pages/work.tsx
+++ pages/work.tsx
@@ -1,94 +1,89 @@
-import React from 'react';
-import { Title, Text, Container, Grid, Link, Button, List } from '@components';
-import { GetStaticProps } from 'next';
-import { getPosts, Post } from '@posts';
-import Head from 'next/head';
+import React from "react";
+import { Title, Text, Container, Grid, Link, Button, List } from "@components";
+import { GetStaticProps } from "next";
+import { getPosts, Post } from "@posts";
+import Head from "next/head";
 
 interface AboutProps {
   experiences: Post[];
 }
 
 const Work = ({ experiences }: AboutProps): JSX.Element => {
-    return (
-      
-      <Container>
-        <Head>
+  return (
+    <Container>
+      <Head>
         <title>work.</title>
       </Head>
-      
-        <Container
-          alignContent="center"
-          alignItems="center"
-          textAlign="center"
-          width="100%"
-          paddingBottom="4rem"
-          gridGap="3rem"
-        >
-          <Title>
-            my experience. 💻
-          </Title>
-          <Container width="100%">
-            {experiences.map(({ data }, i) => (
-              
-                <Grid
-                  key={i}
-                  gridTemplateColumns="1fr 4fr"
-                  justifyItems="flex-start"
-                  gridGap="1rem"
-                  paddingY="2rem"
+
+      <Container
+        alignContent="center"
+        alignItems="center"
+        textAlign="center"
+        width="100%"
+        paddingBottom="4rem"
+        gridGap="3rem"
+      >
+        <Title>my experience. 💻</Title>
+        <Container width="100%">
+          {experiences.map(({ data }, i) => (
+            <Grid
+              key={i}
+              gridTemplateColumns="1fr 4fr"
+              justifyItems="flex-start"
+              gridGap="1rem"
+              paddingY="2rem"
+            >
+              <Container width="100%"></Container>
+              <Grid width="100%" gridTemplateColumns="4fr 1fr">
+                <Container
+                  width="100%"
+                  alignItems="flex-start"
+                  textAlign="start"
                 >
-                  <Container width="100%">
-                  </Container>
-                  <Grid width="100%" gridTemplateColumns="4fr 1fr">
-                    <Container
-                      width="100%"
-                      alignItems="flex-start"
-                      textAlign="start"
+                  <Grid
+                    width="100%"
+                    gridTemplateColumns="repeat(2, auto)"
+                    justifyItems="flex-start"
+                    justifyContent="flex-start"
+                    gridGap="1rem"
+                  >
+                    <Title fontSize="1.5rem" margin={0} as="h3">
+                      <a href={data.link}>{data.title}</a>
+                    </Title>
+                    <Text
+                      fontSize="smaller"
+                      margin={0}
+                      color="rgba(0, 0, 0, 0.1)"
                     >
-                      <Grid
-                        width="100%"
-                        gridTemplateColumns="repeat(2, auto)"
-                        justifyItems="flex-start"
-                        justifyContent="flex-start"
-                        gridGap="1rem"
-                      >
-                        <Title fontSize="1.5rem" margin={0} as="h3">
-                          <a href = {data.link}>{data.title}</a>
-                        </Title>
-                        <Text
-                          fontSize="smaller"
-                          margin={0}
-                          color="rgba(0, 0, 0, 0.1)"
-                        >
-                          {data.date}
-                        </Text>
-                      </Grid>
-                      <Text fontSize="1rem">{data.caption}</Text>
-                      <List marginY="1rem">{data.tags}</List>
-                    </Container>
+                      {data.date}
+                    </Text>
                   </Grid>
-                </Grid>
-            ))}
-            <Link href="img/resume.pdf">
-          <Button>Resume &rarr;</Button>
-        </Link>
-          </Container>
+                  <Text fontSize="1rem">{data.caption}</Text>
+                  <List marginY="1rem">{data.tags}</List>
+                </Container>
+              </Grid>
+            </Grid>
+          ))}
+          <Link href="img/resume.pdf">
+            <Button>Resume &rarr;</Button>
+          </Link>
         </Container>
       </Container>
-    );
+    </Container>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const experiences = await getPosts("experiences");
+  experiences.sort((a, b) =>
+    b.data.year.toString().localeCompare(a.data.year.toString())
+  );
+
+  return {
+    props: {
+      experiences,
+    },
   };
-  
-  export const getStaticProps: GetStaticProps = async () => {
-    const experiences = await getPosts('experiences');
-    experiences.sort((a, b) =>
-      b.data.date.toString().localeCompare(a.data.date.toString()),
-    );
-  
-    return {
-      props: {
-        experiences,
-      },
-    };
-  };
-  
-  export default Work;
+};
+
+export default Work;
